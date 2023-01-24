af449d3b5be976d51fe0d7d739c428542889f9fa
John Sundh <john.sundh@scilifelab.se>
2023-01-24 08:55:43 +0100

    Add parsing of profile

--- src/ductape/cli/run.py
+++ src/ductape/cli/run.py
@@ -16,8 +16,7 @@ import logging
 import subprocess
 import sys
 from pathlib import Path
-from ruamel.yaml import YAML
-
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -30,6 +29,11 @@ def add_arguments(parser):
         type=int,
         help="Run on at most N CPU cores in parallel. Default: Use as many cores as available)",
     )
+    parser.add_argument(
+        "--profile",
+        type=str,
+        help="Run with this profile"
+    )
 
 
 def main(args, arguments):
@@ -38,20 +42,32 @@ def main(args, arguments):
 
 def run_snakemake(
     cores=None,
+    profile=None,
     arguments=None,
 ):
-
+    if profile:
+        if os.path.exists(Path(profile, "config.yaml")):
+            profile_dir = profile
+            logger.debug(f"Running with profile {profile}")
+        else:
+            profile_config = files("ductape").joinpath(f"profiles/{profile}/config.yaml")
+            print(profile_config)
+            profile_dir = os.path.dirname(f"{profile_config}")
+            if not os.path.exists(profile_dir):
+                sys.exit(f"ERROR: Profile {profile} is unknown")
     source = files("ductape").joinpath("Snakefile")
     with as_file(source) as snakefile:
         command = [
             "snakemake",
             f"--cores={'all' if cores is None else cores}",
-            "-p",
             "-s",
             snakefile,
         ]
+        if profile:
+            command += ["--profile", profile_dir]
         if arguments:
             command += arguments
+        print(f"Running: {' '.join(str(c) for c in command)}")
         logger.debug("Running: %s", " ".join(str(c) for c in command))
         exit_code = subprocess.call(command)
 
